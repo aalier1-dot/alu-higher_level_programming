a0c5ac9ca73849ef3e9cf5802ad72546ebc211e6
aalier1-dot <a.alier1@alustudent.com>
2026-06-20 06:48:28 -0700

    Fix 100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,8 @@
-cat > ~/alu-higher_level_programming/python-if_else_loops_functions/100-print_tebahpla.py << 'EOF'
 #!/usr/bin/python3
 s = ""
 for i in range(25, -1, -1):
     if i % 2 != 0:
-        s += "{}".format(chr(ord('a') + i))
+        s += "{}".format(chr(ord("a") + i))
     else:
-        s += "{}".format(chr(ord('A') + i))
+        s += "{}".format(chr(ord("A") + i))
 print("{}".format(s), end="")
