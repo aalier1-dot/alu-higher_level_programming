6158340ad1ce0231b10a6b47956726c5b97d53f9
aalier1-dot <a.alier1@alustudent.com>
2026-07-02 04:47:17 -0700

    Use len()/range() in 3-print_reversed_list_integer.py for standard NoneType exception

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -4,5 +4,5 @@
 
 def print_reversed_list_integer(my_list=[]):
     """Prints all integers of a list, in reverse order"""
-    for number in my_list[::-1]:
-        print("{:d}".format(number))
+    for i in range(len(my_list) - 1, -1, -1):
+        print("{:d}".format(my_list[i]))
