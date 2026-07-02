4d453bcb3510fcf7172d446be0a533683b1fffb7
aalier1-dot <a.alier1@alustudent.com>
2026-07-02 04:57:55 -0700

    Add docstrings to 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""Module for new_in_list"""
+
+
 def new_in_list(my_list, idx, element):
+    """Replaces an element in a list at a specific position without
+    modifying the original list
+    """
     if idx < 0 or idx >= len(my_list):
         return my_list[:]
     new_list = my_list[:]
     new_list[idx] = element
     return new_list
-
