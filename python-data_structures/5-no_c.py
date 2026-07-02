69e32d2000323e344332761fe309e9f584304a78
aalier1-dot <a.alier1@alustudent.com>
2026-07-02 05:14:02 -0700

    Fix PEP8 W391 trailing blank line in 5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    return "".join([i for i in my_string if i != 'c' and i != 'C'])
+"""Module for no_c"""
 
+
+def no_c(my_string):
+    """Removes all characters c and C from a string"""
+    return "".join([i for i in my_string if i != "c" and i != "C"])
