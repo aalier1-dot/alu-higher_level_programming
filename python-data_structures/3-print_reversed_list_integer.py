0958535bf211cd29cb448b24f63e14228e4d8b2c
aalier1-dot <a.alier1@alustudent.com>
2026-07-02 03:39:54 -0700

    Revert to slicing in 3-print_reversed_list_integer.py to preserve expected TypeError for None input

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -4,5 +4,5 @@
 
 def print_reversed_list_integer(my_list=[]):
     """Prints all integers of a list, in reverse order"""
-    for number in reversed(my_list):
+    for number in my_list[::-1]:
         print("{:d}".format(number))
