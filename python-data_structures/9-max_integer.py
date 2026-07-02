b93f278e58301571451037f0a2147ab85a536363
aalier1-dot <a.alier1@alustudent.com>
2026-07-02 13:51:22 -0700

    Fix PEP8 W391 trailing blank line in 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""Module for max_integer"""
+
+
 def max_integer(my_list=[]):
+    """Finds the biggest integer of a list"""
     if len(my_list) == 0:
         return None
-    max_val = my_list[0]
-    for i in my_list:
-        if i > max_val:
-            max_val = i
-    return max_val
-
+    biggest = my_list[0]
+    for number in my_list:
+        if number > biggest:
+            biggest = number
+    return biggest
