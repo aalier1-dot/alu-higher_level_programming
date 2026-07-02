af39eb3c06eba4837ee63895bbe7886510ef04f1
aalier1-dot <a.alier1@alustudent.com>
2026-07-02 11:45:26 -0700

    Fix PEP8 W391 trailing blank line in 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Module for multiple_returns"""
+
+
 def multiple_returns(sentence):
+    """Returns a tuple with the length of a string and its first
+    character
+    """
     if len(sentence) == 0:
         return (0, None)
     return (len(sentence), sentence[0])
-
