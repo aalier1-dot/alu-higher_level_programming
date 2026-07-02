319c8eeccf2f9764bae0be6569e11a9535f2107a
aalier1-dot <a.alier1@alustudent.com>
2026-07-02 03:32:10 -0700

    Add docstrings to 3-print_reversed_list_integer.py

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    for i in my_list[::-1]:
-        print("{:d}".format(i))
+"""Module for print_reversed_list_integer"""
 
+
+def print_reversed_list_integer(my_list=[]):
+    """Prints all integers of a list, in reverse order"""
+    for number in reversed(my_list):
+        print("{:d}".format(number))
