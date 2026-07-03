cffa4b5b33d410ac0ea36d84598f0b43f03c0bef
aalier1-dot <a.alier1@alustudent.com>
2026-07-03 05:33:15 -0700

    Fix 3-print_reversed_list_integer.py: handle None input

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""Module for print_reversed_list_integer"""
+"""Module that prints a list of integers in reverse order."""
 
 
 def print_reversed_list_integer(my_list=[]):
-    """Prints all integers of a list, in reverse order"""
+    """Print all integers of a list, one per line, in reverse order.
+
+    Args:
+        my_list (list): The list of integers to print.
+    """
+    if my_list is None:
+        return
     for i in range(len(my_list) - 1, -1, -1):
         print("{:d}".format(my_list[i]))
