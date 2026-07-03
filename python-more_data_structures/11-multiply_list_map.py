0f07d1f26e2c3af3140c5453ddac648e0e68d688
aalier1-dot <a.alier1@alustudent.com>
2026-07-03 05:40:44 -0700

    Fix 11-multiply_list_map.py: add proper blank line spacing for PEP8

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-"""Module for multiply_list_map"""
-def multiply_list_map(lst=[], n=0): return list(map(lambda x: x*n, lst))
+
+
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
