aef68e5df0919249dbb2a2441d3a000c8a6d9eb3
aalier1-dot <a.alier1@alustudent.com>
2026-07-20 14:30:41 -0700

    Add python-inheritance task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A class that defines base geometry."""
+
+    def area(self):
+        """Raises an Exception."""
+        raise Exception("area() is not implemented")
+
