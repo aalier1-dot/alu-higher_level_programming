703ea4be16f6c95cd57c56f2d33a48d9030f474e
aalier1-dot <a.alier1@alustudent.com>
2026-07-20 07:40:22 -0700

    Fix PEP8 blank lines in 101-locked_class.py

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module that defines a class with restricted instance attributes."""
+
+
 class LockedClass:
     """Class that only allows a first_name instance attribute."""
     __slots__ = ["first_name"]
