d0a12a6a44b20a8a834b32c48ff97de747f7c79e
aalier1-dot <a.alier1@alustudent.com>
2026-07-20 13:55:58 -0700

    Add python-inheritance task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines is_same_class function."""
+
+
+def is_same_class(obj, a_class):
+    """Returns True if obj is exactly an instance of a_class."""
+    return type(obj) is a_class
+
