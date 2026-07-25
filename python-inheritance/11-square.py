51f1c604d4aafa7d20f03232cd5d4a635f7ee41f
aalier1-dot <a.alier1@alustudent.com>
2026-07-25 02:28:53 -0700

    11-square: override __str__ for Square-specific description

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a Square class that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square, inheriting from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new Square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the Square."""
+        return super().area()
+
+    def __str__(self):
+        """Return the string representation of the Square."""
+        return "[Square] {}/{}".format(
+            self._Rectangle__width, self._Rectangle__height)
