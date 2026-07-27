3b9b59527557590ede5edd7ebea27b381da02e90
aalier1-dot <a.alier1@alustudent.com>
2026-07-27 05:33:51 -0700

    Fix 101-add_attribute.py: correct solution for task 13

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,1 +1,12 @@
-aWJ1dGUiKQogICAgc2V0YXR0cihvYmosIG5hbWUsIHZhbHVlKQo= -d /tmp/101aa.b64
+#!/usr/bin/python3
+"""Module that adds a new attribute to an object, if possible."""
+def add_attribute(obj, name, value):
+    """Adds a new attribute to an object, raises TypeError if not possible.
+    Args:
+        obj: the object to add the attribute to.
+        name: the name of the attribute.
+        value: the value of the attribute.
+    """
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
