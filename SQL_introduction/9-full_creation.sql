69978a3972359b80197b2c998c0e36efc8d7801a
aalier1-dot <a.alier1@alustudent.com>
2026-07-29 16:12:24 -0700

    Fix 9-full_creation.sql: single-line printf to avoid newline mangling

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,9 @@
--- creates a table second_table and adds rowsCREATE TABLE IF NOT EXISTS second_table (    id INT,    name VARCHAR(256),    score INT);
+-- creates a table second_table and adds rows
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
 INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
