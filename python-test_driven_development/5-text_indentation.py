9410681b8b6b2f42c46b52d14c32aee1088f559a
aalier1-dot <a.alier1@alustudent.com>
2026-08-01 10:49:51 -0700

    4. Text indentation: text_indentation function with doctest

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""Module that prints text with indentation."""
+"""
+This module contains the function text_indentation(text).
+It prints text with two new lines after each ., ? and : character,
+without any leading or trailing spaces on each printed line.
+"""
 
 
 def text_indentation(text):
-    """Prints text with 2 new lines after . ? and :"""
+    """
+    Prints a text with 2 new lines after each ., ? and : character.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    i = 0
-    while i < len(text):
-        print(text[i], end="")
-        if text[i] in ".?:":
-            print("\n")
-            i += 1
-            while i < len(text) and text[i] == " ":
-                i += 1
+
+    skip_space = False
+    for char in text:
+        if skip_space and char == " ":
             continue
-        i += 1
+        skip_space = False
+        print(char, end="")
+        if char in ".?:":
+            print("\n")
+            skip_space = True
