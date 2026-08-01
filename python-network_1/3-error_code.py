9805555720768c3cf83fe705b3f5404f58a1e72c
aalier1-dot <a.alier1@alustudent.com>
2026-08-01 02:01:16 -0700

    Add 3-error_code.py

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Takes a URL, sends a request, and displays the body of the
+response decoded in utf-8. If an HTTPError occurs, prints the
+error code instead."""
+import sys
+import urllib.error
+import urllib.request
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
