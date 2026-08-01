dad20969ffe31acb64c3fef65dcc943a624f7ba4
aalier1-dot <a.alier1@alustudent.com>
2026-08-01 02:30:22 -0700

    Fix duplicated docstring line in 5-hbtn_header.py

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Takes a URL, sends a request using requests, and displays the
 value of X-Request-Id found in the response header."""
-value of X-Request-Id found in the response header."""
 import requests
 import sys
 if __name__ == "__main__":
